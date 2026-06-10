6f2ab7bee894eb00c4547e33210a5406dd9acab1
Ign6779 <ignacio.deeusebio.cobo@gmail.com>
2026-06-10 20:40:47 +0200

    error bc of empty _iso files

---
+++ src/riscv/rvv/fafft/gf32v_iso.c
@@ -0,0 +1,5 @@
+#include "gf32v_iso.h"
+
+void hqc_rvv_gf32v_iso_placeholder(void) {
+    /* Placeholder until GF(2^32) helpers are implemented. */
+}
---
+++ src/riscv/rvv/fafft/gf32v_iso.h
@@ -0,0 +1,6 @@
+#ifndef GF32V_ISO_H
+#define GF32V_ISO_H
+
+void hqc_rvv_gf32v_iso_placeholder(void);
+
+#endif
