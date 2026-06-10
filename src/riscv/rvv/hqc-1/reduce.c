0f83f3f46d4df657efcf117c3c7aa044ae0e0a1c
Ign6779 <ignacio.deeusebio.cobo@gmail.com>
2026-06-10 20:55:55 +0200

    stupid me for trusting ai code

--- src/riscv/rvv/hqc-1/reduce.c
+++ src/riscv/rvv/hqc-1/reduce.c
@@ -2,17 +2,9 @@
 
 void fafft_reduce(uint64_t *out, const uint64_t *in) {
     /*
-     * Placeholder only.
-     * Real implementation must reduce the FAFFT product modulo x^PARAM_N - 1.
-     *
-     * For now, copy the low PARAM_N words and mask the final partial word.
+     * Dummy placeholder only.
+     * TODO: implement reduction modulo x^PARAM_N - 1.
      */
-
-    for (unsigned i = 0; i < PARAM_N_WORDS; i++) {
-        out[i] = in[i];
-    }
-
-#if (PARAM_N % 64) != 0
-    out[PARAM_N_WORDS - 1] &= (((uint64_t)1 << (PARAM_N % 64)) - 1);
-#endif
+    (void)out;
+    (void)in;
 }
