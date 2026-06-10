a32675a0c9de4b6338bef0246003d9e020ed6823
Ign6779 <ignacio.deeusebio.cobo@gmail.com>
2026-06-10 20:32:37 +0200

    fixing gf16 compile error

---
+++ src/riscv/rvv/fafft/gf16.c
@@ -0,0 +1,5 @@
+#include "gf16.h"
+
+void hqc_rvv_gf16_placeholder(void) {
+    /* Intentionally empty. */
+}
---
+++ src/riscv/rvv/fafft/gf16.h
@@ -0,0 +1,6 @@
+#ifndef GF16_H
+#define GF16_H
+
+void hqc_rvv_gf16_placeholder(void);
+
+#endif
