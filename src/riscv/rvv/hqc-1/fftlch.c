c6cd76ef37b1057e362f3275a26f58dc2966d072
Ign6779 <ignacio.deeusebio.cobo@gmail.com>
2026-06-11 17:09:34 +0200

    dummy fftlch. again.

--- src/riscv/rvv/hqc-1/fftlch.c
+++ src/riscv/rvv/hqc-1/fftlch.c
@@ -11,3 +11,38 @@
 #define HIGH32  UINT64_C(0xffffffff00000000)
 #define FULL64  UINT64_C(0xffffffffffffffff)
 
+void fafft_fftlch_inplace(gf32v_array *a)
+{
+    /*
+     * Dummy placeholder only.
+     * TODO: implement forward FFTLCH in-place.
+     */
+    (void)a;
+}
+
+void fafft_inverse_fftlch_inplace(gf32v_array *a)
+{
+    /*
+     * Dummy placeholder only.
+     * TODO: implement inverse FFTLCH in-place.
+     */
+    (void)a;
+}
+
+void fafft_fftlch(gf32v_array *out, const gf32v_array *in)
+{
+    /*
+     * Dummy placeholder only.
+     * TODO: implement forward FFTLCH.
+     */
+    gf32v_copy(out, in);
+}
+
+void fafft_inverse_fftlch(gf32v_array *out, const gf32v_array *in)
+{
+    /*
+     * Dummy placeholder only.
+     * TODO: implement inverse FFTLCH.
+     */
+    gf32v_copy(out, in);
+}
