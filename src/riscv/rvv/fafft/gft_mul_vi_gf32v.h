556c5d57484bf6b79991acb160728b6086487036
Ign6779 <ignacio.deeusebio.cobo@gmail.com>
2026-06-11 13:02:02 +0200

    new gft_mul_vi_gf32v files that follow data plane structure

---
+++ src/riscv/rvv/fafft/gft_mul_vi_gf32v.h
@@ -0,0 +1,20 @@
+#ifndef FAFFT_GFT_MUL_VI_GF32V_H
+#define FAFFT_GFT_MUL_VI_GF32V_H
+
+#include "fafft_layout.h"
+
+typedef void (*fafft_gft_mul_vi_gf32v_fn)(gf32v_array *out, const gf32v_array *in, unsigned w);
+
+void fafft_gft_mul_v17_gf32v(gf32v_array *out, const gf32v_array *in, unsigned w);
+void fafft_gft_mul_v18_gf32v(gf32v_array *out, const gf32v_array *in, unsigned w);
+void fafft_gft_mul_v19_gf32v(gf32v_array *out, const gf32v_array *in, unsigned w);
+void fafft_gft_mul_v20_gf32v(gf32v_array *out, const gf32v_array *in, unsigned w);
+void fafft_gft_mul_v21_gf32v(gf32v_array *out, const gf32v_array *in, unsigned w);
+void fafft_gft_mul_v22_gf32v(gf32v_array *out, const gf32v_array *in, unsigned w);
+void fafft_gft_mul_v23_gf32v(gf32v_array *out, const gf32v_array *in, unsigned w);
+void fafft_gft_mul_v24_gf32v(gf32v_array *out, const gf32v_array *in, unsigned w);
+void fafft_gft_mul_v25_gf32v(gf32v_array *out, const gf32v_array *in, unsigned w);
+void fafft_gft_mul_v26_gf32v(gf32v_array *out, const gf32v_array *in, unsigned w);
+void fafft_gft_mul_v27_gf32v(gf32v_array *out, const gf32v_array *in, unsigned w);
+
+#endif
